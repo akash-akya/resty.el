94c0d3b1f872761eaa339842e185dc3ea8889bce
akash-akya <akashh246@gmail.com>
2019-08-18 19:30:15 +0530

    Allow only request at once. add timeout option

--- lib/resty.el
+++ lib/resty.el
@@ -10,6 +10,11 @@
   :group 'resty
   :type 'boolean)
 
+(defcustom resty-default-timeout 7
+  "Default timeout for requests in seconds"
+  :group 'resty
+  :type 'integer)
+
 (defcustom resty-buffer-response-name "*HTTP Response*"
   "Name for response buffer."
   :group 'resty
@@ -37,35 +42,56 @@
                    "&"))
     ""))
 
-(defun resty--make-request--new (method path body &rest rest)
+(defun resty--make-request (method path body &rest rest)
   (setq user-headers (or (plist-get rest :headers) '())
         base-url (or (plist-get rest :base-url) (resty--base-url))
-        output (or (plist-get rest :output) t)
-        params (resty--url-params (or (plist-get rest :params) '())))
+        success-handler (resty--create-success-handler (or (plist-get rest :output) t))
+        params (resty--url-params (or (plist-get rest :params) '()))
+        timeout (or (plist-get rest :timeout) resty-default-timeout))
   (let ((headers (append user-headers resty-default-headers))
         (url (concat base-url path params)))
-    (resty--http-do method url '() headers (json-encode-plist body) output)))
+    (resty--http-do
+     method
+     url
+     headers
+     (json-encode-plist body)
+     timeout
+     success-handler)))
 
-(defun resty--http-do (method url params headers entity output)
+(defun resty--create-success-handler (handler)
+  (if (eq handler t)
+      #'resty--success-response-handler
+    (resty--make-succ-callback-handler handler)))
+
+(defvar resty--request-state nil)
+(defvar resty--request-time-start nil)
+
+(defun resty--http-do (method url headers entity timeout success-handler)
   (if resty-log-request
-      (message "HTTP %s %s Headers:[%s] Body:[%s]" method url headers entity))
-  (let ((success-handler
-         (if (eq output t)
-             #'resty--success-response-handler
-           (resty--make-succ-callback-handler output))))
-    (request url
-             :params params
-             :headers headers
-             :type method
-             :data entity
-             :parser 'buffer-string
-             :sync t
-             :timeout 7
-             :success success-handler
-             :error #'resty--error-response-handler)
-    (message "Done")))
+      (message "HTTP %s %s Headers: %s Body: %s" method url headers entity))
+  (if resty--request-state
+      (message "[resty] A request is already in process... skipping")
+    (setq resty--request-state 'attempt)
+    (setq resty--request-time-start (current-time))
+    (unwind-protect
+        (progn
+          (request url
+                   :headers headers
+                   :type method
+                   :data entity
+                   :parser 'buffer-string
+                   ;; :sync t
+                   :timeout timeout
+                   :success success-handler
+                   :error #'resty--error-response-handler)
+          (setq resty--request-state 'started)
+          (message "[resty] Request started"))
+      (when (eq resty--request-state 'attempt) ;; if (request ...) itself fail
+        (setq resty--request-state nil)
+        (message "[resty] Request failed due to an error")))))
 
 (cl-defun resty--success-response-handler (&key data response &allow-other-keys)
+  (resty--cleanup)
   (with-current-buffer (get-buffer-create resty-buffer-response-name)
     (erase-buffer)
     (js-mode)
@@ -81,6 +107,7 @@
     (switch-to-buffer-other-window (current-buffer))))
 
 (cl-defun resty--error-response-handler (&key data response &allow-other-keys)
+  (resty--cleanup)
   (with-current-buffer (get-buffer-create resty-buffer-response-name)
     (erase-buffer)
     (js-mode)
@@ -99,43 +126,53 @@
   (let ((content-type (request-response-header response "Content-Type")))
     (string-prefix-p "application/json" content-type)))
 
+
+(defun resty--cleanup ()
+  (setq resty--request-state nil) ;; FIXME: dont use global variable
+  (message "[resty] Request Completed"))
+
 (defun resty--make-succ-callback-handler (func)
   (setq-local resty--callback func) ;; FIXME: dirty fix, since closure is not working
   (cl-function
    (lambda (&key data response &allow-other-keys)
+     (resty--cleanup)
      (if (json-response? response)
          (funcall resty--callback (json-read-from-string data) response)
        (signal "Not json" response)))))
 
-(defun resty--print (response key)
-  (let ((value (plist-get (request-response-settings response) key)))
-    (insert (format "%s\n" (or value "")))))
 
 (defun resty--print-headers (response)
-  (let ((hstart (point)))
-    (insert "\n\n")
-    (resty--print response :url)
-    (resty--print response :headers)
-    (when (request-response-error-thrown response)
-      (insert (format "%s\n" (request-response-error-thrown response))))
-    (insert "\n")
-    (insert (request-response--raw-header response))
+  (let ((hstart (point))
+        (url (plist-get (request-response-settings response) :url))
+        (request-headers (plist-get (request-response-settings response) :headers))
+        (time (float-time (time-subtract (current-time) resty--request-time-start))))
+    (insert
+     (string-join
+      (mapcar (lambda (s) (format "%s" s))
+              (list "\n"
+                    url
+                    request-headers
+                    (format "Request duration: %fs" time)
+                    (or (request-response-error-thrown response) "")
+                    (request-response--raw-header response)
+                    ))
+      "\n"))
     (comment-region hstart (point))))
 
 (defun POST (path body &rest rest)
-  (apply #'resty--make-request--new (append (list "POST" path body) rest)))
+  (apply #'resty--make-request (append (list "POST" path body) rest)))
 
 (defun GET (path &rest rest)
-  (apply #'resty--make-request--new (append (list "GET" path nil) rest)))
+  (apply #'resty--make-request (append (list "GET" path nil) rest)))
 
 (defun PATCH (path body &rest rest)
-  (apply #'resty--make-request--new (append (list "PATCH" path body) rest)))
+  (apply #'resty--make-request (append (list "PATCH" path body) rest)))
 
 (defun PUT (path body &rest rest)
-  (apply #'resty--make-request--new (append (list "PUT" path body) rest)))
+  (apply #'resty--make-request (append (list "PUT" path body) rest)))
 
 (defun DELETE (path body &rest rest)
-  (apply #'resty--make-request--new (append (list "DELETE" path body) rest)))
+  (apply #'resty--make-request (append (list "DELETE" path body) rest)))
 
 ;; config
 
